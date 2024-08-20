32f6b9d5acf8317c246b42c8c541f76420c5d7c9
Qusai Baker <148033591+mequsaibaker@users.noreply.github.com>
2024-08-20 12:42:07 +0200

    updating project 1

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,19 @@
+import reactImg from "./assets/react-core-concepts.png";
+
+const reactDesc = ["Fundamental", "Crucial", "Core"];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function Header() {
+  const description = reactDesc[getRandomInt(3)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
