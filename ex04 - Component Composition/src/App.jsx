85e22beb44836ff03294242e466a9b66588b0d62
Qusai Baker <148033591+mequsaibaker@users.noreply.github.com>
2024-09-12 18:09:47 +0200

    coding ex04

---
+++ ex04 - Component Composition/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <Card name="Maria Miles">
+      <p>
+        Maria is a professor of Computer Science at the University of Illinois.
+      </p>
+      <p>
+        <a href="mailto:blake@example.com">Email Maria</a>
+      </p>
+    </Card>
+  );
+}
+
+export default App;
---
+++ ex04 - Component Composition/src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = ({ name, children }) => {
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      {children}
+    </div>
+  );
+};
+
+export default Card;
