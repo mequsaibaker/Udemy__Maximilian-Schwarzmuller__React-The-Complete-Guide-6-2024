f40b08db26e216ef04d7994561e840c4b195562b
Qusai Baker <148033591+mequsaibaker@users.noreply.github.com>
2024-09-13 00:27:57 +0200

    updating proj02

---
+++ 02-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const Player = ({ initialName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" value={playerName} onChange={handleChange} required />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={() => setIsEditing((editing) => !editing)}>
+          {isEditing ? "Save" : "Edit"}
+        </button>
+      </span>
+    </li>
+  );
+};
+
+export default Player;
